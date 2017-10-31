09e191d8564be86d8a24aa4d058d21508fa28c6f
Daniel Huffer <danielhuffer@Daniels-MacBook-Pro.local>
2017-11-01 03:42:06 +1000

    http

--- ML/database_setup.py
+++ ML/database_setup.py
@@ -6,15 +6,15 @@ def form_db_connection(coin, endpoint):
     client = MongoClient(port=27017) # But then this will work ?
     db = client.franklin  # Access the franklin database
     # This is not a very elegent solution but I don't know how to pass the db coin as a parameter
-    if coin == "bitcoin":
+    if coin == "USDT-BTC":
         data_source = db.bitcoin
         return data_source
-    if coin == "ltc":
+    if coin == "BTC-LTC":
         data_source = db.ltc
         return data_source
-    if coin == "neo":
+    if coin == "BTC-NEO":
         data_source = db.neo
         return data_source
-    if coin == "ethereum":
+    if coin == "BTC-ETH":
         data_source = db.ethereum
         return data_source
--- ML/profit_hunter.py
+++ ML/profit_hunter.py
@@ -3,6 +3,7 @@ import time
 import process_data
 import database_setup
 import os
+import buy_sell
 
 QUARTHOUR = 15 * 4 # Constant representing the data points per hour (15s intervals)
 
@@ -16,33 +17,42 @@ class mythread(threading.Thread):
 
 
 def thread_work(coin, endpoint):
-    testBuy = 0
+    make_purchase = 0
     profitLoss = 0
     transactionCount = 0
     print("Mongo Endpoint is {0}".format(endpoint))
     while True:
         datasource = database_setup.form_db_connection(coin, endpoint)
-        lastprice, xaxis, meanlast, stdhour, stdupper, stdlower, recentavgprice, recentstd, \
+        last_price, xaxis, meanlast, stdhour, stdupper, stdlower, recentavgprice, recentstd, \
         recentstdupper, recentstdlower = process_data.generate_statlists(datasource, QUARTHOUR)
-        print('Last recorded price', lastprice[-1])
+        print('Last recorded price', last_price[-1])
         print('Last recorded 15min lower bound ', recentstdlower)
         print('Last recorded 15min Upper bound ', recentstdupper)
-        if lastprice[-1] < recentstdlower and testBuy == 0:
+
+        # If the current price has fallen below our threshold, it's time to buy
+        if last_price[-1] < recentstdlower and make_purchase == 0:
             print("Making a purchase")
-            testBuy = lastprice[-1]
-        elif lastprice[-1] >= recentstdupper and testBuy != 0 \
-                and lastprice[-1] > (testBuy * 0.0025): #bittrex trade fee = 0.0025
+            make_purchase = last_price[-1]
+
+            purchase_dict = {'currency': coin, 'OrderType':'LIMIT', 'Quantity': 1.00000000, 'Rate':last_price[-1], 'TimeInEffect':'IMMEDIATE_OR_CANCEL'
+                             ,'ConditionType':'NONE','Target':0}
+            buy_sell.purchase(purchase_dict,"buy")
+
+
+
+        elif last_price[-1] >= recentstdupper and make_purchase != 0 \
+                and last_price[-1] > (make_purchase * 0.0025): #bittrex trade fee = 0.0025
             print("Making a sell")
-            profitLoss += (recentstdupper - (1.0025*testBuy))
+            profitLoss += (recentstdupper - (1.0025*make_purchase))
             transactionCount += 1
-            testBuy = 0
-        elif lastprice[-1] <= testBuy * 0.9 and testBuy != 0:
+            make_purchase = 0
+        elif last_price[-1] <= make_purchase * 0.9 and make_purchase != 0:
             print("Making a sell")
             transactionCount += 1
-            profitLoss += (lastprice[-1] - (1.0025*testBuy))
-            testBuy = 0
-        print('Current Purchase ', testBuy)
-        if testBuy != 0:
+            profitLoss += (last_price[-1] - (1.0025*make_purchase))
+            make_purchase = 0
+        print('Current Purchase ', make_purchase)
+        if make_purchase != 0:
             print('Current Sell Goal', recentstdupper)
         else:
             print('Current Sell Goal', 0)
@@ -54,7 +64,7 @@ def thread_work(coin, endpoint):
 
 if __name__ == "__main__":
     threads = []
-    coins = {0:"ethereum", 1:"bitcoin", 2:"ltc", 3:"neo"}
+    coins = {0:"BTC-ETH", 1:"USDT-BTC", 2:"BTC-LTC", 3:"BTC-NEO"}
     '''
     if os.environ['APP_ENV'] == 'docker':
         print("Sleeping for 20 seconds - waiting for data to be Mongo")
