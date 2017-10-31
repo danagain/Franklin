31b6a5fbb9ebb68e408e1e2071ae5709e0b16366
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-31 14:00:45 +0100

    Adding in buy/sell API that allows a get for balance and a get/sell for each type of currency

--- api/api.js
+++ api/api.js
@@ -1,1 +1,21 @@
-// the api for the web frontend
+"use strict";
+
+const express = require( "express" ),
+    router = require( "./routes/routes" )();
+
+const app = express();
+
+// Routes
+app.use( "/", router );
+
+// Catch all 404's
+app.get( "*", ( req, res ) => {
+    res.status( 404 ).json([{"Not found": true}]);
+} );
+
+// Listener
+const server = app.listen( 3000, () => {
+    console.log( "listening on port 3000" );
+} );
+
+module.exports = server;
---
+++ api/routes/routes.js
@@ -0,0 +1,65 @@
+const express = require( "express" );
+const bittrex = require("node-bittrex-api");
+
+let connectionString;
+if (process.env.APP_ENV === 'docker') {
+  connectionString = "mongodb://mongo:27017/franklin";
+} else {
+  connectionString =
+    "mongodb://franklin:theSEGeswux8stat@ds241055.mlab.com:41055/franklin";
+}
+
+// change this to env vars later
+bittrex.options({
+    apikey: "1b64d15bace644849152c9e42f7091bc",
+    apisecret: "5d392d3589004bf9988b72f10022c509"
+});
+
+const routes = () => {
+
+    // Create Router
+    const router = express.Router();
+
+    router.route( "/api" )
+        .get( ( req, res ) => {
+            res.json([{"version": "0.0.1"}])
+        } );
+    router.route( "/api/balance/:currency" )
+        .get( (req, res) => {
+            bittrex.getbalance({ currency : req.params.currency }, ( data, err ) => {
+                res.json(data)
+              });
+        } );
+    router.route( "/api/buy/:currency" )
+        .post( (req, res) => {
+            bittrex.tradebuy({
+                MarketName: req.params.currency,
+                OrderType: req.OrderType,
+                Quantity: req.Quantity,
+                Rate: req.Rate,
+                TimeInEffect: req.TimeInEffect, // supported options are 'IMMEDIATE_OR_CANCEL', 'GOOD_TIL_CANCELLED', 'FILL_OR_KILL'
+                ConditionType: req.ConditionType, // supported options are 'NONE', 'GREATER_THAN', 'LESS_THAN'
+                Target: req.Target, // used in conjunction with ConditionType
+              }, ( data, err ) => {
+                res.json( data );
+              });
+        } );
+    router.route( "/api/sell/:currency" )
+        .post( (req, res) => {
+            bittrex.tradesell({
+                MarketName: req.params.currency,
+                OrderType: req.OrderType,
+                Quantity: req.Quantity,
+                Rate: req.Rate,
+                TimeInEffect: req.TimeInEffect, // supported options are 'IMMEDIATE_OR_CANCEL', 'GOOD_TIL_CANCELLED', 'FILL_OR_KILL'
+                ConditionType: req.ConditionType, // supported options are 'NONE', 'GREATER_THAN', 'LESS_THAN'
+                Target: req.Target, // used in conjunction with ConditionType
+              }, ( data, err ) => {
+                res.json( data );
+              });
+        } );
+
+    return router;
+};
+
+module.exports = routes;
